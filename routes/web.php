afd1df1931d967d6a5baac55baf49a8fcb79eb86
kazuma.nishimura <bbttbtbt000@gmail.com>
2021-03-29 06:58:36 +0000

    kadai-tasklist

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('Task/{id}', 'TasksController@show');
+// Route::post('Task', 'TasksController@store');
+// Route::put('Task/{id}', 'TasksController@update');
+// Route::delete('Task/{id}', 'TasksController@destroy');
+
+// // index: showの補助ページ
+// Route::get('Task', 'TasksController@index')->name('tasks.index');
+// // create: 新規作成用のフォームページ
+// Route::get('Tasks/create', 'TasksController@create')->name('tasks.create');
+// // edit: 更新用のフォームページ
+// Route::get('Tasks/{id}/edit', 'TasksController@edit')->name('tasks.edit');
+
+
+
