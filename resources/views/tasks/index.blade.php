59879244c632a617f8a9aa252fcae7c981ce7b7d
kazuma.nishimura <bbttbtbt000@gmail.com>
2021-03-29 14:35:45 +0000

    kadai-tasklist

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,7 +16,7 @@
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    <td>{{ $task->id }}</td>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
@@ -24,6 +24,6 @@
         </table>
     @endif
     
-     {!! link_to_route('messages.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
+     {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
 
 @endsection
